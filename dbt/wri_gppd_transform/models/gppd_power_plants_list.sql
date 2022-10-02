bddb604bcd26e3ed537f930c08069a8aabae6874
caldeirav <vincent.caldeira@gmail.com>
2022-10-02 07:53:05 +0000

    Restructure pipeline code for new metadata loading config

--- dbt/wri_gppd_transform/models/gppd_power_plants_list.sql
+++ dbt/wri_gppd_transform/models/gppd_power_plants_list.sql
@@ -1,9 +1,14 @@
-{{ config(materialized='table') }}
+{{
+  config(
+    materialized = 'view',
+    view_security = 'invoker'
+  )
+}}
 
 with source_data as (
    select gppd_idnr,name,country,country_long,latitude,longitude,capacity_mw, year_of_capacity_data, 
     primary_fuel,other_fuel1,other_fuel2,other_fuel3,commissioning_year,owner 
-    from osc_datacommons_dev.sandbox.gppd
+    from osc_datacommons_dev.wri_gppd.gppd_source
 )
 
 select * from source_data
